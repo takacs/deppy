9b7ea56efb36184eef30f7bd7c16051b824bfae8
takacs <davidetkcs@gmail.com>
2023-07-18 17:03:20 +0200

    Add function to read file

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,9 @@
 use std::process::Command;
-use std::collections::HashMap;
 use std::str;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
-#[derive(Debug)]
-struct PipPackage {
-    name: String,
-    version: String,
-    location: String,
-    }
-
-fn retrieve_installed_pip_packages() -> HashMap<String, PipPackage> {
-    let output = Command::new("pip")
-                         .arg("list")
-                         .arg("-v")
-                         .output()
-                         .expect("Failed to run pip list -v.");
-
-    let mut data = String::new();
-    data.push_str(match str::from_utf8(&output.stdout) {
-        Ok(val) => val,
-        Err(_) => panic!("Got Non-UTF-8 input")
-    });
-
-    // println!("stdout:\n{}", String::from_utf8_lossy(&output.stdout));
-
-    let mut package_map: HashMap<String ,PipPackage> = HashMap::new();
-
-    let lines: Vec<&str> = data.lines().collect();
-    for line in lines.iter().skip(2) {
-        let columns: Vec<&str> = line.split_whitespace().collect();
-        if columns.len() >= 3 {
-                let nameval = columns[0].to_string();
-                package_map.insert(nameval, PipPackage {
-                        name:  columns[0].to_string(),
-                        version: columns[1].to_string(),
-                        location: columns[2].to_string(),
-                    });
-                //  println!("{:?}", package_map.get(&columns[0].to_string())); 
-            }
-    }
-    package_map
-}
 
 fn get_python_sys_path_list() -> String {
     let output = Command::new("python")
@@ -56,7 +19,26 @@ fn get_python_sys_path_list() -> String {
     });
     data
 }
+
+fn extract_dependencies_from_file(filename: &str) -> i32 {
+    if let Ok(lines) = read_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                println!("{}", ip);
+            }
+        }
+    }
+    let a = 1;
+    a
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
 fn main() {
-    println!("Output: {}", get_python_sys_path_list());
-    let package_map: HashMap<String, PipPackage> = retrieve_installed_pip_packages();
+    let sys_list: String = get_python_sys_path_list();
+    extract_dependencies_from_file("test.csv");
 }
